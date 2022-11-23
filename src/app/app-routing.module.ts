f3d9b4d53850e7656ca314bee779682fce685ef8
Leoasa1 <leoasa84@gmail.com>
2022-11-22 23:17:12 -0800

    added styles.css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { SignInComponent } from './sign-in/sign-in.component';
 import { RecipeInstructionsPageComponent } from './recipe-instructions-page/recipe-instructions-page.component';
 import { MyPantryComponent } from './my-pantry/my-pantry.component';
 import { SearchPageComponent } from './search-page/search-page.component';
@@ -11,11 +10,14 @@ import { SearchPageComponent } from './search-page/search-page.component';
 const routes: Routes = [
   { path: '', redirectTo: '/recipe-instructions', pathMatch: 'full' },
   { path: 'homepage', component: HomepageComponent },
-  { path: 'signin', component: SignInComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'recipe-instructions', component: RecipeInstructionsPageComponent, data: {id: '000000'}},
+  {
+    path: 'recipe-instructions',
+    component: RecipeInstructionsPageComponent,
+    data: { id: '000000' },
+  },
   { path: 'my-pantry', component: MyPantryComponent },
-  { path: 'search', component: SearchPageComponent }
+  { path: 'search', component: SearchPageComponent },
 ];
 
 @NgModule({
